bab44eb65c2bc8bc4bbe3c4c37eb2e570647fd89
namooplus <namoo.developer@gmail.com>
2026-03-19 17:50:47 +0900

    feat: simplify

--- hw02/shVert.glsl
+++ hw02/shVert.glsl
@@ -5,5 +5,5 @@ layout(location=0) in vec3 aPos;
 uniform vec3 uCenter;
 
 void main() {
-    gl_Position = vec4(uCenter[0] + aPos[0], uCenter[1] + aPos[1], uCenter[2] + aPos[2], 1.0);
+    gl_Position = vec4(uCenter + aPos, 1.0);
 } 
